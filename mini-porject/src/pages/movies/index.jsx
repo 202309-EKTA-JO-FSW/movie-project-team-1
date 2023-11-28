24ee350408264887b6b26131c0a67b67648099a3
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-11-28 17:06:28 +0300

    Redirection from movies list to movie details page

--- mini-porject/src/pages/movies/index.jsx
+++ mini-porject/src/pages/movies/index.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { options ,ImageUrl} from '@/ApiInfo';
 import Pagination from '../../components/Pagination/Pagination'
+import Link from 'next/link';
 
-const MovieList = () => {
+const MovieList = ({MovieCategory="now_playing"}) => {
     const [movies, setMovies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${currentPage}`, options)
+        fetch(`https://api.themoviedb.org/3/movie/${MovieCategory}?language=en-US&page=${currentPage}`, options)
         .then(response => response.json())
         .then((response) => {
             setMovies(response.results);  
@@ -26,7 +27,8 @@ const MovieList = () => {
       <ul className="flex flex-wrap justify-center">
         {movies.map((movie) => (
           <li key={movie.id} className="w-1/4 p-4">
-            <div className="bg-white border rounded-lg shadow-md p-4">
+          <Link href={`/movies/${movie.id}`}>
+             <div className="bg-white border rounded-lg shadow-md p-4">
               <img
                 src={`${ImageUrl}${movie.poster_path}`}
                 alt={movie.title}
@@ -37,6 +39,7 @@ const MovieList = () => {
                 <p className="text-gray-500">{movie.overview}</p>
               </div>
             </div>
+            </Link> 
           </li>
         ))}
       </ul>
