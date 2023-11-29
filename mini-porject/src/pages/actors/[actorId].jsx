71005940b0b774992eaa5f41adaf461a15f0fc02
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-11-29 21:53:49 +0300

    redirecting is done and everything work well

--- mini-porject/src/pages/actors/[actorId].jsx
+++ mini-porject/src/pages/actors/[actorId].jsx
@@ -1,43 +1,110 @@
 import React, {useState, useEffect} from 'react'
 import {options,ImageUrl} from "@/ApiInfo"
 import {useRouter} from 'next/router';
+import Link from 'next/link';
 
-function ActorInfo() {
- const router = useRouter();
- const id  = router.query.actorId;
- //const id = 556356;
- const [actor, setActor] = useState({});
- const [credits, setCredits] = useState({});
- const [allActors, setAllActors] = useState({});
- const [age, setAge] = useState('');
+// function ActorInfo() {
+//  const router = useRouter();
+//  const id  = router.query.actorId;
+//  //const id = 556356;
+//  const [actor, setActor] = useState({});
+//  const [credits, setCredits] = useState({});
+//  const [allActors, setAllActors] = useState({});
+//  const [age, setAge] = useState('');
  
- useEffect(() => {
-    //Single Actor
-    fetch(`https://api.themoviedb.org/3/person/${id}?language=en-US`, options)
-        .then(response => response.json())
-        .then(response =>{ 
-          const birthDate = new Date(response.birthday);
-          const currentDate = new Date();
-          let calculatedAge = currentDate.getFullYear() - birthDate.getFullYear();
-          setAge(calculatedAge);
-          setActor(response)})
-        .catch(err => console.error(err));
-    //actor movies
-    fetch(`https://api.themoviedb.org/3/person/${id}/movie_credits?language=en-US`, options)
-          .then(response => response.json())
-          .then(response => setCredits(response))
-          .catch(err => console.error(err));
-    //All Actors
-    fetch('https://api.themoviedb.org/3/person/popular?language=en-US&page=1', options)
-          .then(response => response.json())
-          .then(response => setAllActors(response))
-          .catch(err => console.error(err));
- }, [])
+//  useEffect(() => {
+//     //Single Actor
+//     fetch(`https://api.themoviedb.org/3/person/${id}?language=en-US`, options)
+//         .then(response => response.json())
+//         .then(response =>{ 
+//           const birthDate = new Date(response.birthday);
+//           const currentDate = new Date();
+//           let calculatedAge = currentDate.getFullYear() - birthDate.getFullYear();
+//           setAge(calculatedAge);
+//           setActor(response)})
+//         .catch(err => console.error(err));
+//     //actor movies
+//     fetch(`https://api.themoviedb.org/3/person/${id}/movie_credits?language=en-US`, options)
+//           .then(response => response.json())
+//           .then(response => setCredits(response))
+//           .catch(err => console.error(err));
+//     //All Actors
+//     fetch('https://api.themoviedb.org/3/person/popular?language=en-US&page=1', options)
+//           .then(response => response.json())
+//           .then(response => setAllActors(response))
+//           .catch(err => console.error(err));
+//  }, [])
 
- const actorPic = (!!actor.profile_path)?`${ImageUrl}${actor.profile_path}`: null;
- const actorName = actor.name;
- const actorGender = (actor.gender === 1)? "Female": "Male";
- //calculate avg popularity
+//  const actorPic = (!!actor.profile_path)?`${ImageUrl}${actor.profile_path}`: null;
+//  const actorName = actor.name;
+//  const actorGender = (actor.gender === 1)? "Female": "Male";
+//  //calculate avg popularity
+//  const allPopularities = (!!allActors.results)? allActors.results.map((acc) =>  acc.popularity ): null;
+//  const sumPopularities = (allPopularities)? allPopularities.reduce((sum, val) => sum + val, 0): null;
+//  const avgPopularities = (sumPopularities)? sumPopularities / allPopularities.length: null;
+//  const popularity = actor.popularity > (avgPopularities - 10)? "High": "Low";
+
+//  const birthday = actor.birthday;
+//  const biography = actor.biography;
+//  //list of movies
+//  const topMovies = (!!credits.cast)? credits.cast.reduce((result, cast) => {
+//       if (result.length < 7 && cast.vote_average >= 6 && cast.popularity > 35) {
+//         result.push(cast);
+//       }
+//       return result;
+//     }, []): null;
+//  const moviesIn = (topMovies)? topMovies.map((movie) => (
+//     <div key={movie.id}>
+//       <Link href={`/movies/${movie.id}`}><img src={`${ImageUrl}${movie.poster_path}`} alt={movie.title} width={"100px"} /></Link>
+//       <div>{movie.title}</div>
+//     </div>
+//   )): null;
+
+
+//   return (
+//     <>
+//         <h1>Actor Information</h1>
+//         <img src={actorPic} alt={actor.name} width={"200px"} />
+//         <h2>{actorName}</h2>
+//         <div><b>Gender:</b> {actorGender}</div>
+//         <div><b>Popularity:</b> {popularity}</div>
+//         <div><b>Birthday:</b> {birthday} ({age} years old)</div>
+//         <div>
+//           <h3>Biography</h3>
+//           <p>{biography}</p>
+//         </div>
+//         <div>
+//           <h3>Known For</h3>
+//           {moviesIn}
+//         </div>
+//     </>
+//   )
+// }
+
+// export default ActorInfo;
+
+
+function ActorInfo({ actor, credits, allActors }) {
+  const router = useRouter();
+  const id = router.query.actorId;
+
+  function calculateAge(){
+    if(actor.deathday === null){
+    const birthDate = new Date (actor.birthday);
+    const currentDate = new Date();
+    const age = birthDate? currentDate.getFullYear() - birthDate.getFullYear(): null;
+    return age;
+    }
+    else{
+      return null;
+    }
+  }
+  const age = calculateAge();
+
+  const actorPic = (!!actor.profile_path) ? `${ImageUrl}${actor.profile_path}` : null;
+  const actorName = actor.name;
+  const actorGender = actor.gender === 1 ? 'Female' : 'Male';
+  //calculate avg popularity
  const allPopularities = (!!allActors.results)? allActors.results.map((acc) =>  acc.popularity ): null;
  const sumPopularities = (allPopularities)? allPopularities.reduce((sum, val) => sum + val, 0): null;
  const avgPopularities = (sumPopularities)? sumPopularities / allPopularities.length: null;
@@ -54,7 +121,7 @@ function ActorInfo() {
     }, []): null;
  const moviesIn = (topMovies)? topMovies.map((movie) => (
     <div key={movie.id}>
-      <img src={`${ImageUrl}${movie.poster_path}`} alt={movie.title} width={"100px"} />
+      <Link href={`/movies/${movie.id}`}><img src={`${ImageUrl}${movie.poster_path}`} alt={movie.title} width={"100px"} /></Link>
       <div>{movie.title}</div>
     </div>
   )): null;
@@ -67,7 +134,7 @@ function ActorInfo() {
         <h2>{actorName}</h2>
         <div><b>Gender:</b> {actorGender}</div>
         <div><b>Popularity:</b> {popularity}</div>
-        <div><b>Birthday:</b> {birthday} ({age} years old)</div>
+        <div><b>Birthday:</b> {birthday} {age?"("+age+" years old"+")": null}</div>
         <div>
           <h3>Biography</h3>
           <p>{biography}</p>
@@ -79,7 +146,30 @@ function ActorInfo() {
     </>
   )
 }
+//
+export async function getServerSideProps(context) {
+  const { actorId } = context.query;
+
+  // Fetch actor data using actorId
+  const actorResponse = await fetch(`https://api.themoviedb.org/3/person/${actorId}?language=en-US`,options);
+  const actor = await actorResponse.json();
+  
+
+  // Fetch actor movie credits
+  const creditsResponse = await fetch(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?language=en-US`,options);
+  const credits = await creditsResponse.json();
+
+  // Fetch all actors for popularity comparison
+  const allActorsResponse = await fetch('https://api.themoviedb.org/3/person/popular?language=en-US&page=1',options);
+  const allActors = await allActorsResponse.json();
+
+  return {
+    props: {
+      actor,
+      credits,
+      allActors,
+    },
+  };
+}
 
 export default ActorInfo;
-
-
--- mini-porject/src/pages/movies/[movieId].jsx
+++ mini-porject/src/pages/movies/[movieId].jsx
@@ -4,93 +4,65 @@ import { useRouter } from 'next/router'
 import Link from 'next/link';
 
 
-function movieInfo(){
-     const router = useRouter()
-     const id = router.query.movieId;
-     console.log(id);
-     //const id = 901362;
-     const [movie, setMovie] = useState({});
-     const [cast, setCast] = useState({});
-     const [similar, setSimilar] = useState({});
-     const [video, setVideo] = useState({});
-    
-      useEffect(() => {
-        //Movie Details
-        fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
-          .then(response => response.json())
-          .then(response => setMovie(response))
-          .catch(err => console.error(err));
-        //Credits
-        fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`, options)
-          .then(response => response.json())
-          .then(response => setCast(response))
-          .catch(err => console.error(err));
-        //Similar
-        fetch(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`, options)
-          .then(response => response.json())
-          .then(response => setSimilar(response))
-          .catch(err => console.error(err));
-        //Video
-        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
-          .then(response => response.json())
-          .then(response => setVideo(response))
-          .catch(err => console.error(err));
-      }, []);
-      
-      
-    
-      
-      const poster =(!!movie.poster_path)? `${ImageUrl}${movie.poster_path}`:null;
-      const name = movie.original_title;
-      const release = movie.release_date;
-      const movieYear = (release)? release.substring(0, 4): null;
-      const runTime = movie.runtime;
-      const language = movie.original_language;
-      const rating = Math.ceil(movie.vote_average * 10);
-      const votes = movie.vote_count;
-      const director = (!!cast.crew)? cast.crew.filter((dir) => dir.job === "Director"): null;
-      const overview = movie.overview;
-      let trailer = (!!video.results)? video.results.filter((trailer) => trailer.name === "Official Trailer"): null;
+function movieInfo({ movie, cast, similar, video }) {
+  const {poster_path ,
+         title: name,
+         release_date: release,
+         runtime: runTime,
+         vote_average,
+         vote_count: votes
+        } = movie;
+
+  const poster = `${ImageUrl}${poster_path}`;
+  const language = movie.spoken_languages[0].english_name;
+  const movieYear = release ? release.substring(0, 4) : null;
+  const rating = Math.ceil(vote_average * 10);
+  const director = (!!cast.crew)? cast.crew.filter((dir) => dir.job === "Director"): null;
+  const overview = movie.overview;
+  let trailer = (!!video.results)? video.results.filter((trailer) => trailer.name === "Official Trailer"): null;
       if(trailer == null){
         trailer = (!!video.results)? video.results.filter((trailer) => trailer.official === true): null;
       }
-      const offTrailer = trailer !=null ? trailer[0]!=null ? trailer[0].key: null: null;
-      const companyName = (!!movie.production_companies) && movie.production_companies.length>0? movie.production_companies[0].name: null;
-      const companyImg = (!!movie.production_companies) && movie.production_companies.length>0 ? movie.production_companies[0].logo_path: null;
-      
-      const listDir = director? director.map((dir) => (
-        <div key={dir.id}>
-            <p>{dir.name}</p>
-        </div>
-      )): null;
-
-      const actingCast = (!!cast.cast)?cast.cast.filter((cast) => cast.known_for_department === "Acting" && cast.order < 5): null
-
-      const listCast = actingCast? actingCast.map( actor => (
-        <div key={actor.id}>
-          <img src={`${ImageUrl}${actor.profile_path}`} alt={actor.name} width={"100px"}/>
-          <p>{actor.name}</p>
-        </div>
-      )): null;
-
-      const similarMovies = (!!similar.results)? similar.results.map(movie => (
-        <div key={movie.id}>
-            {(!!movie.poster_path) &&  <Link href={`/movies/${movie.id}`}><img src={`${ImageUrl}${movie.poster_path}`} alt={movie.title} width={"100px"}/></Link>}
-            <p>{movie.title}</p>
-        </div>
-      )): null;
+  const offTrailer = trailer !=null ? trailer[0]!=null ? trailer[0].key: null: null;
+  const companyName = (!!movie.production_companies) && movie.production_companies.length>0? movie.production_companies[0].name: null;
+  const companyImg = (!!movie.production_companies) && movie.production_companies.length>0 ? movie.production_companies[0].logo_path: null;
+  
+  const listDir = director? director.map((dir) => (
+    <div key={dir.id}>
+        <p>{dir.name}</p>
+    </div>
+  )): null
+  const actingCast = (!!cast.cast)?cast.cast.filter((cast) => cast.known_for_department === "Acting" && cast.order < 5): null;
+  const listCast = actingCast? actingCast.map( actor => (
+    <div key={actor.id}>
+      <Link href={`../actors/${actor.id}`} ><img src={`${ImageUrl}${actor.profile_path}`} alt={actor.name} width={"100px"}/></Link>
+      <p>{actor.name}</p>
+    </div>
+  )): null
+  const topMovies = (!!similar.results)? similar.results.reduce((result, movie) => {
+    if(result.length < 7 && movie.vote_average > 7){
+      result.push(movie);
+    }
+    return result;
+  }, []): null;
+  const similarMovies = (topMovies)? topMovies.map((movie) => (
+    <div key={movie.id}>
+        {(!!movie.poster_path) &&  <Link href={`/movies/${movie.id}`}><img src={`${ImageUrl}${movie.poster_path}`} alt={movie.title} width={"100px"}/></Link>}
+        <p>{movie.title}</p>
+    </div>
+  )): null;
 
     return (
         <>
             <h1>Single Movie Page</h1>
             <img src={poster} alt="movie poster" width="200px"/>
             <h2>{name} ({movieYear}) </h2>
-            <p>{release}</p>
-            <p>{runTime} Minutes</p>
-            <p>Language: {language}</p>
-            <p>Rating: {rating}%</p>
-            <p>Votes: {votes}</p>
-            <p>Director:</p>
+            <p><b>Release Date:</b> {release}</p>
+            <p><b>Duration:</b> {runTime} Minutes</p>
+            <p><b>Language:</b> {language}</p>
+            <p><b>Rating:</b> {rating}%</p>
+            <p><b>Votes:</b> {votes}</p>
+            <p><b>Director(s):</b></p>
             {listDir}
             <div>
                 <h3>Overview</h3>
@@ -111,6 +83,48 @@ function movieInfo(){
         </>
     )
 }
+export async function getServerSideProps(context) {
+  const { movieId } = context.query;
 
+  try {
+    // Movie Details
+    const movieResponse = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+      options
+    );
+    const movie = await movieResponse.json();
+
+    // Credits
+    const creditsResponse = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`,
+      options
+    );
+    const cast = await creditsResponse.json();
+
+    // Similar
+    const similarResponse = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`,
+      options
+    );
+    const similar = await similarResponse.json();
+
+    // Video
+    const videoResponse = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      options
+    );
+    const video = await videoResponse.json();
+
+    return {
+      props: { movie, cast, similar, video },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true,
+    };
+  }
+}
 export default movieInfo;
+
 
