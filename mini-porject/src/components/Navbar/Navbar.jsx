16ec9ab5687bd53e95c5347fdf88f7a2bd6ce3af
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-11-29 15:30:31 +0300

    Link navbar with pages

--- mini-porject/src/components/Navbar/Navbar.jsx
+++ mini-porject/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,24 @@ import Link from 'next/link';
 import { options } from '../../ApiInfo';
 
 function Navbar()  {
-    const movieCategory = ['Top Rate', 'Popular', 'Latest', 'Now Playing', 'Upcoming'];
+    const movieCategory = [
+        {
+            catrogeValue :'top_rated',
+           navbarValue:'Top Rate'
+        },
+        {
+            catrogeValue :'popular',
+            navbarValue:'Popular'
+        },
+        {
+            catrogeValue :'now_playing',
+            navbarValue:'Now Playing'
+        },
+        {
+            catrogeValue :'upcoming',
+            navbarValue:'Upcoming'
+        }
+    ];
     const[genres,SetGenres]=useState([]);
   
     //const genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western'];
@@ -22,16 +39,23 @@ function Navbar()  {
             <div className='dropdown'>
                 <Link href='/' className='dropdown-link'>Movies</Link>
                 <div className='dropdown-content'>
-                    {movieCategory.map((category,index) => <Link href={`../../Pages/movies`} MovieCategory={category} key={category+index}>{category}</Link>)}
+                    {movieCategory.map((item,index) => <Link  href={{
+            pathname: '/movies',
+            query: { MovieCategory: `${item.catrogeValue}` },
+          }}  key={item.catrogeValue+index}>{item.navbarValue}</Link>)}
                 </div>
             </div>
             <div className='dropdown'>
                 <Link href='/' className='dropdown-link'>Genres</Link>
                 <div className='dropdown-content'>
-                    {genres.map((genre) =>  <Link href={`../../Pages/movies`} key={genre.id}>{genre.name}</Link>)}
+                    {genres.map((genre) =>  <Link href={{
+            pathname: '/movies',
+            query: { GenreID: `${genre.id}` },
+          }}
+          key={genre.id}>{genre.name}</Link>)}
                 </div>
             </div>
-            <Link href='/'>Actors</Link>
+            <Link href='/actors'>Actors</Link>
         </nav>
     )
 };
