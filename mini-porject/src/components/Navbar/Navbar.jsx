23a7c03065ad52c8e9dcc1fcf0276ab37d2d39e6
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-11-29 14:37:01 +0300

    Update Navbar

--- mini-porject/src/components/Navbar/Navbar.jsx
+++ mini-porject/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Link from 'next/link';
+import { options } from '../../ApiInfo';
 
 function Navbar()  {
     const movieCategory = ['Top Rate', 'Popular', 'Latest', 'Now Playing', 'Upcoming'];
-
-    const genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western'];
-
+    const[genres,SetGenres]=useState([]);
+  
+    //const genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western'];
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/genre/movie/list?language=en`, options)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            SetGenres(response.genres);  
+        })
+        .catch(err => console.error(err));
+    }, []);
     return (
         <nav>
             <Link href='/'>Logo</Link>
             <div className='dropdown'>
                 <Link href='/' className='dropdown-link'>Movies</Link>
                 <div className='dropdown-content'>
-                    {movieCategory.map((category) => <a href='' key={category}>{category}</a>)}
+                    {movieCategory.map((category,index) => <Link href={`../../Pages/movies`} MovieCategory={category} key={category+index}>{category}</Link>)}
                 </div>
             </div>
             <div className='dropdown'>
                 <Link href='/' className='dropdown-link'>Genres</Link>
                 <div className='dropdown-content'>
-                    {genres.map((genre) => <a href='' key={genre}>{genre}</a>)}
+                    {genres.map((genre) =>  <Link href={`../../Pages/movies`} key={genre.id}>{genre.name}</Link>)}
                 </div>
             </div>
             <Link href='/'>Actors</Link>
